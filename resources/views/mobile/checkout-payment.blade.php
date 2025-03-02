e135eb74ee747474e8fc003f8c8466d0b1ec1741
AlbertWebs <albertmuhatia@gmail.com>
2025-03-02 16:58:01 +0300

    update checkout

--- resources/views/mobile/checkout-payment.blade.php
+++ resources/views/mobile/checkout-payment.blade.php
@@ -1,13 +1,12 @@
 @extends('mobile.master-home')
 
 @section('content')
-<div class="padding_bottom">
-     <!-- Checkout Start -->
-     <div class="panel-body">
-        {!!html_entity_decode($iframe)!!}
+    <div class="padding_bottom">
+        <!-- Checkout Start -->
+        <div class="panel-body">
+            {!!html_entity_decode($iframe)!!}
+        </div>
+        <!-- Checkout End -->
     </div>
-    <!-- Checkout End -->
-</div>
-@include('mobile.horizontal-nav')
-    @include('mobile.main-nav')
+    @include('mobile.horizontal-nav')
 @endsection
